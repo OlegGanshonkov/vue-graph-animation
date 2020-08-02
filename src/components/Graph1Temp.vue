cf0d00a3814825a393a0a329f18a43b78f2309ea
olaccursed <olaccursed@yandex.ru>
2020-08-02 14:29:14 +0300

    first graph absolutly ready

--- src/components/Graph1Temp.vue
+++ src/components/Graph1Temp.vue
@@ -5,15 +5,13 @@
 
         <h1>starcraft 2 <span>10 years</span></h1>
 
-        <div class="winnersInfo">Winners:</div>
         <div class="winners">
             <div v-show="winner.visible" :key="index" class="winner" v-for="(winner, index) in winners">
                 <img :src="require(`@/assets/static/${winner.img}`)" width="79" height="60"/>
-                <span class="name">{{winner.name}}</span>
-                <span>{{winner.prize}}</span>
+                <span>{{winner.name}}</span>
             </div>
-        </div>
 
+        </div>
         <div class="xDix">
             <h3 ref="x"></h3>
         </div>
@@ -49,7 +47,7 @@
                 <td class="right"></td>
             </tr>
         </table>
-        <div class="xInfo">Participants RO16:</div>
+        <div class="xInfo">Participants</div>
 
 
         <div v-if="!hideControl" class="control">
@@ -76,7 +74,7 @@
                 stepsCount: 0,
                 hideControl: false,
                 animation: null,
-                duration: 5000,
+                duration: 2000,
                 wrapHeight: null, // Высота парента линии
                 wrapAnim: false,
                 x: null, // Ось X
@@ -84,62 +82,24 @@
                     '2012 <span>StarCraft II World Championship Series: Global Finals</span>',
                     '2013 <span>StarCraft II World Championship Series: Global Finals</span>',
                     '2014 <span>StarCraft II World Championship Series: Global Finals</span>',
-                    '2015 <span>StarCraft II World Championship Series: Global Finals</span>',
-                    '2016 <span>StarCraft II World Championship Series: Global Finals</span>',
-                    '2017 <span>StarCraft II World Championship Series: Global Finals</span>',
-                    '2018 <span>StarCraft II World Championship Series: Global Finals</span>',
-                    '2019 <span>StarCraft II World Championship Series: Global Finals</span>',
                 ],
                 maxXValue: 800,
-                maxLineValue: 9,
+                maxLineValue: 600,
                 winners: [
                     {
                         img: 'protos.png',
-                        name: 'PartinG',
-                        prize: '$100,000',
-                        visible: false
+                        name: 'Stats',
+                        visible:false
+                    },
+                    {
+                        img: 'zerg.png',
+                        name: 'Bly',
+                        visible:false
                     },
                     {
                         img: 'protos.png',
-                        name: 'sOs',
-                        prize: '$100,000',
-                        visible: false
-                    },
-                    {
-                        img: 'zerg.png',
-                        name: 'Life',
-                        prize: '$100,000',
-                        visible: false
-                    },
-                    {
-                        img: 'protos.png',
-                        name: 'sOs',
-                        prize: '$100,000',
-                        visible: false
-                    },
-                    {
-                        img: 'terran.png',
-                        name: 'ByuN',
-                        prize: '$200,000',
-                        visible: false
-                    },
-                    {
-                        img: 'zerg.png',
-                        name: 'Rogue',
-                        prize: '$280,000',
-                        visible: false
-                    },
-                    {
-                        img: 'zerg.png',
-                        name: 'Serral',
-                        prize: '$280,000',
-                        visible: false
-                    },
-                    {
-                        img: 'zerg.png',
-                        name: 'Dark',
-                        prize: '$210,000',
-                        visible: false
+                        name: 'Stats',
+                        visible:false
                     },
                 ],
                 lines: [
@@ -152,7 +112,7 @@
                         width: null,
                         top: null,
                         steps: [
-                            6, 5, 6, 7, 7, 3, 7, 6, 6
+                            400, 600, 450
                         ],
                         stepsValue: [],
                     },
@@ -165,7 +125,7 @@
                         width: null,
                         top: null,
                         steps: [
-                            8, 2, 6, 4, 7, 8, 5, 6, 6
+                            500, 200, 300
                         ],
                         stepsValue: [],
                     },
@@ -178,7 +138,7 @@
                         width: null,
                         top: null,
                         steps: [
-                            2, 9, 4, 5, 2, 5, 4, 4, 4
+                            450, 500, 600
                         ],
                         stepsValue: [],
                     }
@@ -243,7 +203,7 @@
             animate() {
                 const vm = this;
 
-                vm.animation = vm.$anime
+                    vm.animation = vm.$anime
                     .timeline({
                         autoplay: false,
                         update: function () {
@@ -274,11 +234,14 @@
                                 duration: vm.duration,
                                 easing: 'linear',
                                 width: stepValue,
-                                complete: function () {
+                                complete: function() {
                                     vm.stepsCount++;
-                                    if (vm.stepsCount % 3 === 0) {
-                                        let visibleKey = vm.stepsCount / 3 - 1;
-                                        vm.winners[visibleKey].visible = true;
+                                    if (vm.stepsCount === 3){
+                                        vm.winners[0].visible = true;
+                                    } else if (vm.stepsCount === 6){
+                                        vm.winners[1].visible = true;
+                                    } else if (vm.stepsCount === 9){
+                                        vm.winners[2].visible = true;
                                     }
                                 }
                             }, start)
@@ -344,33 +307,22 @@
     .container {
         height: 100%;
     }
-
     .xDix span {
         color: #cccccc;
         font-size: 14px;
     }
-
     .xInfo {
         color: #cccccc;
         width: 800px;
-        margin: 0 auto;
+        margin:0 auto;
         padding-top: 10px;
         font-size: 12px;
-    }
-
-    .winnersInfo {
-        color: #cccccc;
-        margin-left: 30px;
-        padding-top: 10px;
-        font-size: 12px;
-        clear: both;
     }
 </style>
 <style scoped>
     .winners {
-        padding: 15px 10px 10px 10px;
+        padding: 35px 10px 10px 10px;
     }
-
     .winner {
         float: left;
         margin-right: 20px;
@@ -382,16 +334,8 @@
 
     .winner span {
         margin-left: 20px;
-        color: #02ff24;
-        font-size: 12px;
-        display: block;
-    }
-
-    .winner span.name {
-        font-size: 14px;
-        margin-top: 10px;
         color: #fff;
-        display: block;
+        font-size: 12px;
     }
 
     h1 {
@@ -431,21 +375,12 @@
         margin: 0 auto;
     }
 
-    .main .left, .main .right, .main .top, .main .bottom {
-        opacity: 0.7;
-    }
-
-    .main .top .center div {
-        opacity: 0.5;
-        display: none;
-    }
-
     .main .left, .main .right {
         width: 5px;
     }
 
     .main .top {
-        height: 1px;
+        height: 16px;
     }
 
     .main .content .left {
@@ -454,7 +389,6 @@
 
     .main .content .right {
         background: #ccc url('~@/assets/borderRight.png') center repeat-y;
-        display: none;
     }
 
     .main .top .right, .main .top .left {
@@ -466,11 +400,10 @@
         z-index: 2;
         top: 0px;
         left: -11px;
-        width: 15px;
+        width: 16px;
         height: 20px;
         background: transparent url('~@/assets/cornerTopRight.png') center;
         opacity: 0.7;
-        display: none;
     }
 
     .main .top .left div {
@@ -478,11 +411,10 @@
         z-index: 2;
         top: 0px;
         left: 0px;
-        width: 15px;
+        width: 16px;
         height: 20px;
         background: transparent url('~@/assets/cornerTopLeft.png') center;
         opacity: 0.9;
-        display: none;
     }
 
     .main .top .center div {
@@ -493,7 +425,6 @@
         z-index: 2;
         height: 20px;
         background: transparent url('~@/assets/topCenter.png') center repeat-x;
-        display: none;
     }
 
     .main .top .center span {
@@ -505,26 +436,24 @@
         z-index: 2;
         height: 10px;
         background: rgba(0, 118, 231, 0.3);
-        display: none;
     }
 
     .main .bottom .center {
         z-index: 2;
         height: 1px;
-        /*background: transparent url('~@/assets/bottomCenter.png') center repeat-x;*/
-        opacity: 0.5;
+        background: transparent url('~@/assets/bottomCenter.png') center repeat-x;
     }
 
     .main .bottom .right {
-        /*background: transparent url('~@/assets/bottomCenter.png') center repeat-x;*/
+        background: transparent url('~@/assets/bottomCenter.png') center repeat-x;
     }
 
     .main .bottom .left {
-        /*background: transparent url('~@/assets/bottomCenter.png') center repeat-x;*/
+        background: transparent url('~@/assets/bottomCenter.png') center repeat-x;
     }
 
     .main .content .center {
-        background: rgba(0, 118, 231, 0.2);
+        background: rgba(0, 118, 231, 0.3);
     }
 
     .control {
@@ -566,23 +495,23 @@
         margin: 0px;
         background: transparent url('~@/assets/static/bgLineBlue.png') left center repeat-x;
         background-size: 5px 30px;
-        background-size: cover;
+        border-top-right-radius: 50px;
+        border-bottom-right-radius: 50px;
     }
 
     .graph > div.line3 {
         background: transparent url('~@/assets/static/bgLineRed.png') left center repeat-x;
-        background-size: cover;
-
+        background-size: 5px 30px;
     }
 
     .graph > div.line2 {
         background: transparent url('~@/assets/static/bgLineZerg.png') left center repeat-x;
-        background-size: cover;
+        background-size: 5px 30px;
     }
 
     /* line content */
     .graph > div > div {
-        width: 95px;
+        width: 50px;
         height: 50px;
         display: table-cell;
         vertical-align: middle;
@@ -616,5 +545,63 @@
         margin-top: -20px;
     }
 
+    /*
+    .control {
+        width: 810px;
+        margin: 10px auto;
+    }
+    .xDix {
+        padding: 10px;
+        width: 810px;
+        margin: 20px auto;
+    }
+    .graph {
+        position: relative;
+        background: #f4f4f4;
+        border: 1px solid #ccc;
+        width: 810px;
+        height: 300px;
+        margin: 0 auto;
+        padding: 10px;
+    }
+
+    // line wrap
+    .graph > div {
+        position: absolute;
+        border: 1px solid #ccc;
+        padding: 5px;
+        margin: 0px;
+        background: #fff;
+    }
+
+    // line content
+    .graph > div > div {
+        width: 50px;
+        height: 50px;
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .graph > div.line1 > div {
+        background: red;
+    }
+
+    .graph > div.line2 > div {
+        background: coral;
+    }
+
+    .graph > div.line3 > div {
+        background: blue;
+    }
+    .graph > div.line4 > div {
+        background: #42b983;
+    }
+
+    button {
+        padding: 5px 10px;
+        margin: 0px 5px;
+        cursor: pointer;
+    }
+    */
 </style>
 
